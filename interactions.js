1f93fc87c6c5755531bad65a8b94e8880541dd81
saulecabrera <saulecabrera@gmail.com>
2015-01-10 18:01:47 -0600

    interactions.js changes

--- interactions.js
+++ interactions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.col-md-2#gallery').on('click', function(){
+    $('.col-md-2#gallery').on('click', function(){
 		window.open('https://www.flickr.com/photos/126012464@N07/', 'open_window');
 	});
 });
